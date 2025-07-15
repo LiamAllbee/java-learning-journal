cc13f0217c4f26abe34e29afa3d346d597107f87
LiamAllbee <yoshilink1@gmail.com>
2025-07-14 21:57:09 -0400

    Add TextBlockTest.java

---
+++ src/Javanotes/Section_2/TextBlockTest.java
@@ -0,0 +1,13 @@
+package Javanotes.Section_2;
+
+public class TextBlockTest {
+    public static void main(String[] args) {
+        String poem = """
+            As I was walking down the stair,
+                   I met a man who wasn't there.
+                He wasn't there again today.
+                   I wish, I wish he'd go away!""";
+
+        System.out.println(poem);
+    }
+}
